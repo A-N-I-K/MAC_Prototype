983b008a47970bfffd13a7d654659810dab3f93f
Anik <anikynetix@gmail.com>
2016-02-27 17:16:04 -0500

    File reader

--- MAC_Prototype/MAC_Prototype/Source.cpp
+++ MAC_Prototype/MAC_Prototype/Source.cpp
@@ -4,6 +4,17 @@
 
 using namespace std;
 
+const int COLUMNS = 5;
+
+class User {
+public:
+	string fname;
+	string lname;
+	string uid;
+	string pwd;
+	string role;
+};
+
 int main()
 {
 	//cout << "This is a test!" << endl;
@@ -12,14 +23,19 @@ int main()
 	char output[100];
 	if (myReadFile.is_open()) {
 		while (!myReadFile.eof()) {
-
-
 			myReadFile >> output;
-			cout << output;
-
-
+			cout << output << endl;
 		}
 	}
+	else {
+		cout << "File not found!" << endl;
+	}
 	myReadFile.close();
+	system("pause");
 	return 0;
+}
+
+User authUser(string uid, string pwd) {
+	User newuser;
+	return newuser;
 }
