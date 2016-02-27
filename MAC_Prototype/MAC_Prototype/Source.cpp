f9dfa01a0045a6ece2b2d6cb759b408a9bd02668
Anik <anikynetix@gmail.com>
2016-02-27 00:22:19 -0500

    Ignore List Fix

--- MAC_Prototype/MAC_Prototype/Source.cpp
+++ MAC_Prototype/MAC_Prototype/Source.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "This is a test!" << endl;
-	system("pause");
+	//cout << "This is a test!" << endl;
+	ifstream myReadFile;
+	myReadFile.open("user.db");
+	char output[100];
+	if (myReadFile.is_open()) {
+		while (!myReadFile.eof()) {
+
+
+			myReadFile >> output;
+			cout << output;
+
+
+		}
+	}
+	myReadFile.close();
 	return 0;
 }
