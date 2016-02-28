4e514e3b1cc74203514d6c630e836ec141672973
Anik <anikynetix@gmail.com>
2016-02-27 20:21:20 -0500

    listUser() completed

--- MAC_Prototype/MAC_Prototype/Source.cpp
+++ MAC_Prototype/MAC_Prototype/Source.cpp
@@ -17,13 +17,17 @@ public:
 };
 
 User authUser(char user[], char pass[]);
+void listUser();
 void addUser(User userObj);
 
 int main()
 {
 	char user[100];
 	char pass[100];
+	char choice[100];
 	User userObj;
+	bool session;
+	session = true;
 	cout << "Please provide your username :" << endl;
 	cin >> user;
 	cout << "Please provide your password :" << endl;
@@ -31,6 +35,19 @@ int main()
 	userObj = authUser(user, pass);
 	if (userObj.valid) {
 		cout << "Welcome, " << userObj.fname << " " << userObj.lname << "!" << endl;
+		while (session) {
+			cout << "What would you like to do today?" << endl;
+			cout << "1. List users" << endl;
+			cout << "2. Add user" << endl;
+			cout << "3. Modify user" << endl;
+			cout << "4. Delete user" << endl;
+			cout << "5. View log" << endl;
+			cout << "6. Log out" << endl;
+			cin >> choice;
+			if (strcmp(choice, "1") == 0 || strcmp(choice, "One") == 0 || strcmp(choice, "one") == 0) {
+				listUser();
+			}
+		}
 	}
 	else {
 		cout << "Invalid username or password. This incident will be reported." << endl;
@@ -44,13 +61,13 @@ User authUser(char user[], char pass[]) {
 	userObj.valid = false;
 	bool found;
 	found = false;
-	ifstream file;
-	file.open("user.db");
 	char fname[100];
 	char lname[100];
 	char uid[100];
 	char pwd[100];
 	char role[100];
+	ifstream file;
+	file.open("user.db");
 	if (file.is_open()) {
 		while (!file.eof()) {
 			file >> fname;
@@ -74,6 +91,30 @@ User authUser(char user[], char pass[]) {
 	}
 	file.close();
 	return userObj;
+}
+
+void listUser() {
+	char output[100];
+	ifstream file;
+	file.open("user.db");
+	if (file.is_open()) {
+		while (!file.eof()) {
+			file >> output;
+			cout << output << "\t";
+			file >> output;
+			cout << output << "\t";
+			file >> output;
+			cout << output << "\t";
+			file >> output;
+			cout << output << "\t";
+			file >> output;
+			cout << output << "\t" << endl;
+		}
+	}
+	else {
+		cout << "Unable to access user database!" << endl;
+	}
+	file.close();
 }
 
 void addUser(User uobject) {
