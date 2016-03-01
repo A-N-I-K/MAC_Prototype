1c8111f826548db4b31652e59f46fd85c28ad550
Rashmie Abeysinghe <rashmie0045@gmail.com>
2016-03-01 03:58:11 -0500

    queryCheck converted

--- AR-MAC DB/AR-MAC DB/Program.cs
+++ AR-MAC DB/AR-MAC DB/Program.cs
@@ -28,5 +28,29 @@ namespace AR_MAC_DB
         public string pwd;
         public string perm;
         public bool valid;
+
+        public static void queryCheck(string query)
+        {
+            query = query.Replace(" ", String.Empty);
+            query = query.ToLower();
+            int from = query.IndexOf("from");
+            int where = query.IndexOf("where");
+            //Console.WriteLine(query);
+            //Console.WriteLine(where);
+
+            string tables = query.Substring(from + 4, where - from - 4);
+            //Console.WriteLine(tables);
+
+            char[] delimiters = new char[] { ',' };
+            string[] parts = tables.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                //pass parts[i] i.e one of the table names to a function which checks the database to whether the user has permission to access the table.
+            }
+
+        }
+
+
     }
 }
