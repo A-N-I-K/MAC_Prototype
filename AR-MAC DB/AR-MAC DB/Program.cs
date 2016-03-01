21582105b85e4e04cb228d8fc7eed6b4a531e06c
Anik <anikynetix@gmail.com>
2016-03-01 00:53:23 -0500

    Removed listUsers() from Program.cs

--- AR-MAC DB/AR-MAC DB/Program.cs
+++ AR-MAC DB/AR-MAC DB/Program.cs
@@ -3,39 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
 namespace AR_MAC_DB
 {
-    class UserClass
-    {
-        public string fname;
-        public string lname;
-        public string uid;
-        public string pwd;
-        public string permission;
-        public bool valid;
-
-        public void listUser()
-        {
-            try
-            {
-                System.IO.StreamReader file = new System.IO.StreamReader("user.db");
-                string line;
-                while ((line = file.ReadLine()) != null)
-                {
-                    Console.WriteLine(line);
-                }
-            }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine("File not found!!!");
-            }
-
-        }
-
-    }
-
     static class Program
     {
         /// <summary>
