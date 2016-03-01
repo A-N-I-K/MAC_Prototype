fea230093a78f748721042e87f7a6062e87cd7c0
Anik <anikynetix@gmail.com>
2016-03-01 00:06:28 -0500

    Added buttons in SO_Form

--- AR-MAC DB/AR-MAC DB/SO_Form.Designer.cs
+++ AR-MAC DB/AR-MAC DB/SO_Form.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.welcomeLabel = new System.Windows.Forms.Label();
+            this.listUsersButton = new System.Windows.Forms.Button();
+            this.addUserButton = new System.Windows.Forms.Button();
+            this.modifyUserButton = new System.Windows.Forms.Button();
+            this.deleteUserButton = new System.Windows.Forms.Button();
+            this.viewLogButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // welcomeLabel
@@ -40,11 +46,71 @@
             this.welcomeLabel.TabIndex = 0;
             this.welcomeLabel.Text = "Welcome!";
             // 
+            // listUsersButton
+            // 
+            this.listUsersButton.Location = new System.Drawing.Point(233, 124);
+            this.listUsersButton.Name = "listUsersButton";
+            this.listUsersButton.Size = new System.Drawing.Size(75, 23);
+            this.listUsersButton.TabIndex = 1;
+            this.listUsersButton.Text = "List Users";
+            this.listUsersButton.UseVisualStyleBackColor = true;
+            // 
+            // addUserButton
+            // 
+            this.addUserButton.Location = new System.Drawing.Point(233, 153);
+            this.addUserButton.Name = "addUserButton";
+            this.addUserButton.Size = new System.Drawing.Size(75, 23);
+            this.addUserButton.TabIndex = 2;
+            this.addUserButton.Text = "Add User";
+            this.addUserButton.UseVisualStyleBackColor = true;
+            // 
+            // modifyUserButton
+            // 
+            this.modifyUserButton.Location = new System.Drawing.Point(233, 182);
+            this.modifyUserButton.Name = "modifyUserButton";
+            this.modifyUserButton.Size = new System.Drawing.Size(75, 23);
+            this.modifyUserButton.TabIndex = 3;
+            this.modifyUserButton.Text = "Modify User";
+            this.modifyUserButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteUserButton
+            // 
+            this.deleteUserButton.Location = new System.Drawing.Point(233, 211);
+            this.deleteUserButton.Name = "deleteUserButton";
+            this.deleteUserButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteUserButton.TabIndex = 4;
+            this.deleteUserButton.Text = "Delete User";
+            this.deleteUserButton.UseVisualStyleBackColor = true;
+            // 
+            // viewLogButton
+            // 
+            this.viewLogButton.Location = new System.Drawing.Point(233, 240);
+            this.viewLogButton.Name = "viewLogButton";
+            this.viewLogButton.Size = new System.Drawing.Size(75, 23);
+            this.viewLogButton.TabIndex = 5;
+            this.viewLogButton.Text = "View Log";
+            this.viewLogButton.UseVisualStyleBackColor = true;
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(233, 269);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(75, 23);
+            this.logoutButton.TabIndex = 6;
+            this.logoutButton.Text = "Log Out";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            // 
             // soForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(763, 522);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.viewLogButton);
+            this.Controls.Add(this.deleteUserButton);
+            this.Controls.Add(this.modifyUserButton);
+            this.Controls.Add(this.addUserButton);
+            this.Controls.Add(this.listUsersButton);
             this.Controls.Add(this.welcomeLabel);
             this.Name = "soForm";
             this.Text = "SO_Form";
@@ -56,5 +122,11 @@
         #endregion
 
         private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Button listUsersButton;
+        private System.Windows.Forms.Button addUserButton;
+        private System.Windows.Forms.Button modifyUserButton;
+        private System.Windows.Forms.Button deleteUserButton;
+        private System.Windows.Forms.Button viewLogButton;
+        private System.Windows.Forms.Button logoutButton;
     }
 }
