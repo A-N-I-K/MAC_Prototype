1dc116f65c53da779b72dd4e65b073b669487f7e
Anik <anikynetix@gmail.com>
2016-03-01 13:36:12 -0500

    Query parser added

--- AR-MAC DB/AR-MAC DB/Program.cs
+++ AR-MAC DB/AR-MAC DB/Program.cs
@@ -29,48 +29,7 @@ namespace AR_MAC_DB
         public string perm;
         public bool valid;
 
-        public void queryCheckSelect(string query)
-        {
-            query = query.Replace(" ", String.Empty);
-            query = query.ToLower();
-            int from = query.IndexOf("from");
-            int where = query.IndexOf("where");
-           
-
-            string tables = query.Substring(from + 4, where - from - 4);
-            
-
-            char[] delimiters = new char[] { ',' };
-            string[] parts = tables.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
-
-            for (int i = 0; i < parts.Length; i++)
-            {
-                //pass parts[i] i.e one of the table names to a function which checks the database  whether the user has permission to access the table.
-            }
-
-        }
-
-        public void queryCheckInsert(string query)
-        {
-            query = query.Replace(" ", String.Empty);
-            query = query.ToLower();
-            int into = query.IndexOf("into");
-            int bracket = query.IndexOf("(");
-
-
-            string table = query.Substring(into + 4, bracket - into - 4);
-
-            //pass table to a function which checks the database whether the user has permission to access the table.
-
-        }
-
-        public string queryType(string query)
-        {
-            query = query.Replace(" ", String.Empty);
-            query = query.ToLower();
-            string type = query.Substring(0, 6);            
-            return type;
-        }
+        
 
 
     }
--- AR-MAC DB/AR-MAC DB/User_Form.cs
+++ AR-MAC DB/AR-MAC DB/User_Form.cs
@@ -17,13 +17,57 @@ namespace AR_MAC_DB
             InitializeComponent();
         }
 
+        public void queryCheckSelect(string query)
+        {
+            query = query.Replace(" ", String.Empty);
+            query = query.ToLower();
+            int from = query.IndexOf("from");
+            int where = query.IndexOf("where");
+
+
+            string tables = query.Substring(from + 4, where - from - 4);
+
+
+            char[] delimiters = new char[] { ',' };
+            string[] parts = tables.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < parts.Length; i++)
+            {
+                //pass parts[i] i.e one of the table names to a function which checks the database  whether the user has permission to access the table.
+            }
+
+        }
+
+        public void queryCheckInsert(string query)
+        {
+            query = query.Replace(" ", String.Empty);
+            query = query.ToLower();
+            int into = query.IndexOf("into");
+            int bracket = query.IndexOf("(");
+
+
+            string table = query.Substring(into + 4, bracket - into - 4);
+
+            //pass table to a function which checks the database whether the user has permission to access the table.
+
+        }
+
+        public string queryType(string query)
+        {
+            query = query.Replace(" ", String.Empty);
+            query = query.ToLower();
+            string type = query.Substring(0, 6);
+            return type;
+        }
+
         private void commandTextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == (char)Keys.Enter)
             {
-                //consoleListBox.Items.Add(commandTextBox.Text);
+                string type = queryType(commandTextBox.Text);
                 consoleListBox.Items.Insert(0, commandTextBox.Text);
-                commandTextBox.Text=String.Empty;
+                //consoleListBox.Items.Insert(0, type); //For query analysis
+                commandTextBox.Text = String.Empty;
             }
         }
     }
